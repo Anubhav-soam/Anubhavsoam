00a6204910ca33ed9cc2bd5edf1e48e6fae56547
Anubhav-soam <47381136+Anubhav-soam@users.noreply.github.com>
2026-03-12 01:22:05 +0530

    Update blog.config.example.js

--- blog.config.example.js
+++ blog.config.example.js
@@ -2,7 +2,7 @@
 // blog.config.js is ignored by git so you can keep keys private.
 window.BLOG_CLOUD_CONFIG = {
   url: 'https://siktkhguuriujksehvoy.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpa3RraGd1dXJpdWprc2Vodm95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNDgyNzMsImV4cCI6MjA4ODgyNDI3M30.j-RgqbJNxq1Lh-2bGq7ywVrszsOc0RLoYKFnbQwFXz4',
+  anonKey: 'sb_publishable_TWk9qxiciXlM4Cd3qR61Ww_t2wjcf0i',
   table: 'blog_state',
   stateId: 'main'
 };
